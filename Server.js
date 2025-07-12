8033a292c297eecaef0943843cad0b5227ef703b
danidan <danid93575@gmail.com>
2025-07-12 10:50:28 -0700

    first commit

---
+++ Server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import cors from "cors";
+import dotenv from 'dotenv'
+import Contact from "./models/Contact.js";
+import mongoose from "mongoose";
+ 
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5001
+
+app.use(cors());
+app.use(express.json());
+
+// db config
+
+mongoose.connect(process.env.MONGO_URI, {
+     useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log("✅ MongoDB connected!"))
+.catch((err) => console.error("❌ MongoDB failed:", err))
+
+
+  // api config
+
+app.post('/api/register', async (req, res) => {
+  const {name, email, password, message} = req.body;
+  
+  try{
+
+    const newContact = new Contact({name, email, password, message});
+    await newContact.save();
+    res.status(200).json({success: true, data:newContact})
+  } catch(error) {
+      console.error("Error saving contact:", error)
+      res.status(500).json({success: false, message: "Sometime went wrong!"})
+  }
+})
+
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
