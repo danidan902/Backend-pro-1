06f90c3d6bcc57f44c937dcf37b4951b88ac44b9
Daniel Sheleme <danid93575@gmail.com>
2025-07-26 13:11:25 -0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 5001
 
 app.use(cors({
-  origin: "https://danidanportfolio.vercel.app",
+  origin: "https://daniregistration.vercel.app",
   methods: ["GET", "POST", "OPTIONS"],
   allowedHeaders: ["Content-Type"],
   credentials: true
